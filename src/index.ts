5994796d4e47cd709595ff17f9b2037cd7dbf3bf
Johan <johan@macair2020>
2021-03-18 18:42:38 +0100

    small fixes

--- src/index.ts
+++ src/index.ts
@@ -2,22 +2,22 @@ import LibServices from './product-service';
 import app from './app';
 
 // port on which the application will be run, default is 3000
-const port = process.env.PORT ? Number(process.env.PORT) || 3000;
+const port = process.env.PORT ? Number(process.env.PORT) : 3000;
 
 const subscribe = async () => {
   try {
     const productService = await LibServices.Product.subscribe(false); // workflow: use true
 
     if (productService.error) {
-      console.error(r.message);
-    } else {  
+      console.error(productService.message);
+    } else {
       console.log('Refreshed product service using app token');
       // LibServices.I18n.saveAll();
     }
   } catch (_err) {
     console.log('something went wrong while initializing product service');
   }
-}
+};
 
 const startApp = async () => {
   subscribe();
