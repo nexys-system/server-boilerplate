20d3961e15f252878b774df1dbf49051f822cb09
John B <johan@nexys.ch>
2022-02-10 23:05:23 +0100

    Update and rename src/model.ts to src/common/generated/index.ts

--- src/common/generated/index.ts
+++ src/common/generated/index.ts
@@ -1,3 +1,5 @@
+// this is an empty model. It needs to be updated with the actual model
+// run `yarn getAssets` to generate
 import { Entity } from '@nexys/core/dist/query/model/type';
 
 const model: Entity[] = [];
