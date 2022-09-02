a96b4c84ceefa89cb33ad03c9f204329e97b10ec
John B <john@nexys.ch>
2022-09-02 15:36:12 +0200

    upgrade core

--- src/product.ts
+++ src/product.ts
@@ -1,12 +1,11 @@
-import * as FetchR from '@nexys/fetchr';
+import FetchR from '@nexys/fetchr';
+import Cache from '@nexys/node-cache';
 
 import ProductService from '@nexys/core/dist/product';
 import QueryService from '@nexys/core/dist/query/service';
 import WorkflowService from '@nexys/core/dist/services/workflow';
 import EmailService from '@nexys/core/dist/services/email';
 import NotificationService from '@nexys/core/dist/services/notification';
-
-import Cache from '@nexys/node-cache';
 
 import * as Config from './config';
 import model from './common/generated';
@@ -15,7 +14,7 @@ export const email = new EmailService(Config.context);
 export const workflow = new WorkflowService(Config.context);
 export const notifications = new NotificationService(Config.context);
 
-const fetchR = new FetchR.default(Config.database, model);
+const fetchR = new FetchR(Config.database, model);
 export const qs = new QueryService(fetchR);
 export const cache = new Cache();
 
